8e5b11cdde5d8553ca87ea1c13819ff82bf03b9a
James Knox <jamesknox361@gmail.com>
2024-08-26 17:39:39 +0200

    Add project files.

---
+++ ABC-RETAIL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABC_RETAIL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
