294ecca85f9fcfd474daa949e39ffebe021e171b
James Knox <jamesknox361@gmail.com>
2024-08-26 19:32:21 +0200

    Added queue and table service

--- ABC-RETAIL/Services/TableService.cs
+++ ABC-RETAIL/Services/TableService.cs
@@ -1,6 +1,26 @@
-﻿namespace ABC_RETAIL.Services
+﻿using Azure;
+using Azure.Data.Tables;
+using ABC_RETAIL.Models;
+using System.Threading.Tasks;
+
+
+namespace ABC_RETAIL.Services
 {
     public class TableService
     {
+        private readonly TableClient _tableClient;
+
+        public TableService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureStorage:ConnectionString"];
+            var serviceClient = new TableServiceClient(connectionString);
+            _tableClient = serviceClient.GetTableClient("CustomerProfiles");
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddEntityAsync(CustomerProfile profile)
+        {
+            await _tableClient.AddEntityAsync(profile);
+        }
     }
 }
