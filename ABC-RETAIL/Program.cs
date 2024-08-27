48fde3e8bfd96c939f4e7a26d593a9372f0e6246
James Knox <jamesknox361@gmail.com>
2024-08-27 14:24:08 +0200

    Registered services in program.cs

--- ABC-RETAIL/Program.cs
+++ ABC-RETAIL/Program.cs
@@ -1,7 +1,15 @@
+using ABC_RETAIL.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<BlobService>();
+builder.Services.AddSingleton<TableService>();
+builder.Services.AddSingleton<QueueService>();
+builder.Services.AddSingleton<FileService>();
+
 
 var app = builder.Build();
 
