c603989e76b2868e596090e0791538eb9e83ed9e
James Knox <jamesknox361@gmail.com>
2024-08-27 14:41:47 +0200

    Added tasks to home controller.

--- ABC-RETAIL/Controllers/HomeController.cs
+++ ABC-RETAIL/Controllers/HomeController.cs
@@ -25,9 +25,43 @@ namespace ABC_RETAIL.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
         {
-            return View();
+            if (file != null)
+            {
+                using var stream = file.OpenReadStream();
+                await _blobService.UploadBlobAsync("multimedia", file.FileName, stream);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCustomerProfile(CustomerProfile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                await _tableService.AddEntityAsync(profile);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProcessOrder(string orderId)
+        {
+            await _queueService.SendMessageAsync("order-processing", $"Processing order {orderId}");
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadContract(IFormFile file)
+        {
+            if (file != null)
+            {
+                using var stream = file.OpenReadStream();
+                await _fileService.UploadFileAsync("contracts", file.FileName, stream);
+            }
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
